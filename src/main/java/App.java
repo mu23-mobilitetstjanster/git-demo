4e7c66af1244aba9f8571ef9290d4555aa4e7df8
Jakob Rolandsson <rolandsson.jakob@gmail.com>
2023-11-23 10:25:05 +0100

    changed initial output to an application description

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,6 @@
 public class App {
 
   public static void main(String[] args) {
-    System.out.println("Hey Gertrude!!");
-    System.out.println("You are awesome!");
+    System.out.println("Automated system for complementing your name");
   }
 }
