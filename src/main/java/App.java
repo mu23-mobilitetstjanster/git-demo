2023371ac216bfa33d4aae9549b017b4a9e82423
Jakob Rolandsson <rolandsson.jakob@gmail.com>
2023-11-23 10:27:41 +0100

    removed old outputs

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,9 +3,6 @@ import java.util.Scanner;
 public class App {
 
   public static void main(String[] args) {
-    System.out.println("Hey Gertrude!!");
-    System.out.println("You are awesome!");
-
     String name = query("What is your name?");
     System.out.println("What a beautiful name " + name);
   }
