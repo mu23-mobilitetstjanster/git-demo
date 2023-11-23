84c9da6b5df38b836fc201d9f49b6fe233002bf8
Jakob Rolandsson <rolandsson.jakob@gmail.com>
2023-11-23 10:21:36 +0100

    added new query user from console feature

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
+
 public class App {
 
   public static void main(String[] args) {
     System.out.println("Hey Gertrude!!");
     System.out.println("You are awesome!");
+
+    String name = query("What is your name?");
+    System.out.println("What a beautiful name " + name);
+  }
+
+  static String query(String message) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print(message + " ");
+    return scanner.nextLine();
   }
 }
